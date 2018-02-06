3e2213c4f98a00e195bd50cda2e37e5c251e2eec
Abhay Ruparel <abhayruparel2000@gmail.com>
2018-02-06 12:27:04 +0000

    Remove AudioFX
    Change-Id: I9f30f4a19830f46e01d402674abb42595c7896d6
    Signed-off-by: Abhay Ruparel <abhayruparel2000@gmail.com>

--- product/audio.mk
+++ product/audio.mk
@@ -18,3 +18,7 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
+
+# Remove packages that do not work well
+PRODUCT_PACKAGES += \
+    RemovePackages
