626b6dafc15908c0f88a6c514a936001bbc39c7e
lzzy12 <jhashivam2020@gmail.com>
2018-02-18 13:30:00 +0000

    Move all configs to system/vendor/etc
    Signed-off-by: lzzy12 <jhashivam2020@gmail.com>

--- product/audio.mk
+++ product/audio.mk
@@ -16,7 +16,7 @@
 
 # Audio Configs
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/vendor/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/vendor/etc/mixer_paths_0.xml
 
 # Remove packages that do not work well
