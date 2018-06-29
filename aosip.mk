5685ead8b3df516e6a61d2f7c890884e816483a1
lzzy12 <jhashivam2020@gmail.com>
2018-06-29 14:08:21 +0000

    AOSIP-ify
    Signed-off-by: lzzy12 <jhashivam2020@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -21,10 +21,10 @@ $(call inherit-product, device/samsung/j7velte/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Device identifier
-PRODUCT_NAME := lineage_j7velte
+PRODUCT_NAME := aosip_j7velte
 PRODUCT_DEVICE := j7velte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo lineage_j7velte-userdebug
+add_lunch_combo aosip_j7velte-userdebug
